74dd80d7b5e1992a8b65f93cdc3beb4f0211ffe5
Aniket Kaimal <aniket1995@yahoo.co.in>
2023-05-06 22:43:50 +0530

    Change x to bottleNum

--- 1-dummy_project/BottleSong.java
+++ 1-dummy_project/BottleSong.java
@@ -1,21 +1,21 @@
 public class BottleSong {
   public static void main(String[] args) {
-    int x = 10;
+    int bottlesNum = 10;
     String verse = " green bottles hanging on the wall,";
 
-    while(x > 1) {
-      System.out.println(x + verse);
-      System.out.println(x + verse);
+    while(bottlesNum > 1) {
+      System.out.println(bottlesNum + verse);
+      System.out.println(bottlesNum + verse);
       System.out.println("And if one green bottle should accidentally fall,");
-      x = x- 1;
+      bottlesNum = bottlesNum- 1;
       System.out.print("There will be ");
-      System.out.println(x + " green bottles hanging on the wall." );
+      System.out.println(bottlesNum + " green bottles hanging on the wall." );
       System.out.println(" ");
     }
 
-    if (x ==1){
-      System.out.println(x + verse);
-      System.out.println(x + verse);
+    if (bottlesNum ==1){
+      System.out.println(bottlesNum + verse);
+      System.out.println(bottlesNum + verse);
       System.out.println("And if this green bottle should accidentally fall,");
       System.out.println("There will be no green bottles hanging on the wall.");
     }
