80945e1fd7aa4f633bd64daa068833512cc4471b
Aniket Kaimal <aniket1995@yahoo.co.in>
2023-05-14 02:00:56 +0530

    Add a new repo

---
+++ 2-apnaCollegeTuts/1-variables/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        System.out.println("The table of the number "+ number + " is:");
+        System.out.println(number*1);
+        System.out.println(number*2);
+        System.out.println(number*3);
+        System.out.println(number*4);
+        System.out.println(number*5);
+        System.out.println(number*6);
+        System.out.println(number*7);
+        System.out.println(number*8);
+        System.out.println(number*9);
+        System.out.println(number*10);
+    }
+} 
